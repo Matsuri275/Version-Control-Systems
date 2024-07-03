073e96f591b494c4ef9951122a655fd9035434bd
matsunaga <matsunagaryoichiro@gmail.com>
2024-07-03 19:43:30 +0900

    HoT: initial revision

--- initial revision.c
+++ initial revision.c
@@ -3,21 +3,22 @@
 #include <time.h>
 
 char* toss_coin() {
-    return rand() % 2 == 0 ? "表" : "裏";
+    return rand() % 2 == 0 ? "Heads" : "Tails";
 }
 
-int main(){
-    char name[50];
-    printf("コインを投げています...\n");
+int main() {
+ printf("Tossing a coin...\n");
     srand(time(0));
     int heads_count = 0, tails_count = 0;
 
     for (int i = 1; i <= 3; i++) {
         char* result = toss_coin();
-        printf("ラウンド%d: %s\n", i, result);
-        if (result == "表") heads_count++;
+        printf("Round %d: %s\n", i, result);
+        if (result == "Heads") heads_count++;
         else tails_count++;
     }
 
-    printf("表: %d, 裏: %d\n", heads_count, tails_count);
-}
+    printf("Heads: %d, Tails: %d\n", heads_count, tails_count);
+
+    return 0;
+}   
